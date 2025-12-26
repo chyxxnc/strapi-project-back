73d25dd87e813dd2c912d4f23ad8e1fa2c3fdeb6
chyxxnc <ahnchaeyeon7@gmail.com>
2025-12-26 14:50:23 +0900

    컬럼 추가 및 콘텐츠 필드만 표시되도록 수정

--- src/api/form/controllers/form.ts
+++ src/api/form/controllers/form.ts
@@ -10,8 +10,12 @@ export default factories.createCoreController('api::form.form', ({strapi}) => ({
             const model = strapi.getModel('api::form.form');
             const fields = model.attributes;
 
+            // 전체 컬럼 출력
+            console.log(model);
+
             const filteredFields = Object.fromEntries(
-                Object.entries(fields).filter(([key]) => ['title', 'memo'].includes(key))
+                // 시스템 필드 제거 (콘텐츠 필드만 보이도록)
+                Object.entries(fields).filter(([key]) => !['createdAt', 'updatedAt', 'publishedAt', 'createdBy', 'updatedBy', 'locale', 'localizations'].includes(key))
             );
 
             ctx.body = {fields: filteredFields};
